08515cf7f095ca5afdb504971a99ba5a7265f797
jesus carrion <jesusdcarriong@gmail.com>
2022-04-25 23:42:25 -0400

    cambios

--- js/app.js
+++ js/app.js
@@ -127,3 +127,34 @@ function eliminarCafeLocalStorage(cafe) {
 function vaciarLocalStorage() {
     localStorage.clear();
 }
+
+document.addEventListener('keyup', e=>{
+
+    if (e.target.matches('#buscador')){
+        document.querySelectorAll('.card').forEach(cafe =>{
+            cafe.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?cafe.classList.remove("filtro")
+            :cafe.classList.add('filtro')
+        })
+    }
+  
+})
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+  
---
+++ js/sweealert.js
@@ -0,0 +1,10 @@
+Swal.fire({
+    title: '¡Bienvenido!',
+    text: '¡te estabamos esperando, pide ya tu cafe favorito aqui!',
+    icon: 'info',
+    confirmButtonText: 'continuar',
+    confirmButtonColor: 'green',
+    padding: '5rem',
+    backdrop: true,
+    timer: 5000,
+});
